5d2d0b1e13a98bd00d82c3927996981b98bdf1f9
Stephanie Miles <118240368+stephaniemiless@users.noreply.github.com>
2023-12-08 22:17:14 -0600

    Math Object

---
+++ src/Math Object/index.js
@@ -0,0 +1,39 @@
+// Math = built-in object that provides a collection of properties
+//        and methods.
+
+// ceil always rounds up
+// floor rounds down
+// truncate = eliminates decimal portions
+
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 3.99;
+let y = 2;
+let z;
+
+// round = 4
+// ceil = 4
+// floor = 3
+// trunc = 3
+
+//trig = sin, cos, tan
+
+z = Math.trunc(x);
+console.log("ceil function: " + z);
+
+console.log(Math.pow(3, y));
+
+console.log(Math.sqrt(81));
+
+console.log(Math.log(10));
+
+console.log(Math.tan(45));
+
+console.log(Math.abs(-3.21));
+
+console.log(Math.sign(-32));
+
+console.log(Math.max(x, y, z));
+
+console.log(Math.min(x, y, z));
